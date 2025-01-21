f7dc960423d1a5e8538790f3b8b409294722ad90
waliuzair75 <waliuzair75@hotmail.com>
2025-01-20 23:51:37 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Game Developer");
+        System.out.println("Why: Entertainment");
+        System.out.println("Median salary: $130,160");
+
+    }
+}
