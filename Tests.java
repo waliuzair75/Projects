fd8285fbfbacbb4b3dbddac6a841b3a35ba89152
waliuzair75 <waliuzair75@hotmail.com>
2025-01-26 20:58:52 -0500

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,12 @@
+public class TestScores {
+    public static void main(String[] args) {
+        double testScore1 = 88.2;
+        double testScore2 = 78.9;
+        double testScore3 = 97.6;
+        double average = (testScore1 + testScore2 + testScore3) / 3;
+        System.out.println("Test Score 1: " + testScore1);
+        System.out.println("Test Score 2: " + testScore2);
+        System.out.println("Test Score 3: " + testScore3);
+        System.out.println("The average of 3 test scores is: " + average);
+    }
+}
